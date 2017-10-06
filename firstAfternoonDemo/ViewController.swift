0e439972ade9c3c7bd1e53f02b833724806104f1
Sharp <rsha7868@740s-pl-127.local>
2017-10-06 13:27:31 -0600

    Making random colors are so much fun.

--- firstAfternoonDemo/ViewController.swift
+++ firstAfternoonDemo/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController
 
     @IBOutlet weak var firstButton: UIButton!
     
+    @IBAction func firstMethod(_ sender: Any)
+    {
+        if (firstButton.backgroundColor == .orange )
+        {
+            firstButton.backgroundColor = .black
+        }
+        else
+        {
+            firstButton.backgroundColor = .orange
+        }
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redValue :CGFloat  = CGFloat(Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 255.00)
+        let blueValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 255.00)
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
+    }
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
