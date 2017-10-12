e3eb3a503df91e01de478271a4a6a795425506df
Sharp <rsha7868@740s-pl-127.local>
2017-10-12 13:11:21 -0600

    Second screen to the main area.

---
+++ SecondController.swift
@@ -0,0 +1,10 @@
+//
+//  SecondController.swift
+//  firstAfternoonDemo
+//
+//  Created by Sharp, Ryan on 10/12/17.
+//  Copyright © 2017 Sharp, Ryan. All rights reserved.
+//
+
+import Foundation
+
