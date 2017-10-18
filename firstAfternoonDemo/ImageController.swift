25b68a788462835c038bd20067a2f3da404e8848
Sharp <rsha7868@740s-pl-127.local>
2017-10-18 13:55:09 -0600

    still fixing the data.

--- firstAfternoonDemo/ImageController.swift
+++ firstAfternoonDemo/ImageController.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2017 Sharp, Ryan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBOutlet weak var piture: UIImageView!
+@IBOutlet weak var imageButton: UIButton!
+@IBOutlet weak var SoundSlider: UISlider!
+
+@IBAction func imageButton(_ sender: Any) {
+}
+
+
+private func changeimage() -> Void
+{
+    if (imageCounter > 2)
+    {
+        imageCounter = 0)
+    }
+    
+    if (imageCounter == 0)
+    {
+        imageFrame.image = UIImage(name: "Atomic.Bomb.jpg")
+    }
+    else if (imageCounter == 2)
+    {
+        imageFrame.image = UIImage(name: "Missle.1-1")
+    }
+    else
+    {
+        imaggeFrame.image = UIImage(named: "Nuke.jpg")
+    }
+    imageCounter += 1
+    }
+
+}
