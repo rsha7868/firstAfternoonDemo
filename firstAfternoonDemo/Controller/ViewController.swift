05da71514024f657a41e6272e08abf3bfc21d152
Sharp <rsha7868@740s-pl-127.local>
2017-10-24 13:03:34 -0600

    How are there 3 changes? i fixed 29 errors!!!!Weird.

--- firstAfternoonDemo/Controller/ViewController.swift
+++ firstAfternoonDemo/Controller/ViewController.swift
@@ -6,17 +6,18 @@
 //  Copyright © 2017 Sharp, Ryan. All rights reserved.
 //
 
-import UIKit
+import UIKitss
 
 class ViewController: UIViewController
 {
 private lazy var colorTools : ColorTools = ColorTools()
     @IBOutlet weak var firstButton: UIButton!
     
-    @IBOutlet weak var piture: UIImageView!
     @IBOutlet weak var imageButton: UIButton!
-    @IBOutlet weak var SoundSlider: UISlider!
-   
+    @IBOutlet weak var soundButton: UIButton!
+    
+    @IBOutlet weak var soundSlider: UISlider!
+    
         @IBAction func firstMethod(_ sender: UIButton)
         
     {
@@ -46,31 +47,85 @@ private lazy var colorTools : ColorTools = ColorTools()
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func imageButton(_ sender: Any) {
+    @IBAction func imageButton(_ sender: Any)
+    {
     }
     
 
-    private func changeimage() -> Void
-    {
-        if (imageCounter > 2)
-        {
-            imageCounter = 0)
-        }
+    //private func changeImage() -> Void
+    //{
+        //if (imageCounter > 3)
+        //{
+            //imageCounter = 0)
+        //}
         
-        if (imageCounter == 0)
-        {
-            imageFrame.image = UIImage(name: "Atomic.Bomb.jpg")
-        }
-        else if (imageCounter == 2)
-        {
-            imageFrame.image = UIImage(name: "Missle.1-1")
-        }
-        else
-        {
-            imaggeFrame.image = UIImage(named: "Nuke.jpg")
-        }
-        imageCounter += 1
+        //if (imageCounter == 0)
+        //{
+            //imageFrame.image = UIImage(name: "Atomic.Bomb.jpg")
+        //}
+        //else if (imageCounter == 2)
+        //{
+            //imageFrame.image = UIImage(name: "Missle.1-1")
+        //}
+        //else
+        //{
+            //imaggeFrame.image = UIImage(named: "Nuke.jpg")
+        //}
+        //imageCounter += 1
+    //
+    
+    
+    @IBAction func soundButton() -> Void
+    {
+        playMusicFile()
+        view.backgroundColor = color.createRandomColor()
     }
+    @IBAction func soundSlider() -> Void
+    {
+        let seekTime = Double (soundSlider.value)
+        soundPlayer?.currentTime = seekTime
+    }
+     private func playMusicFile() -> Void
+     {
+        if let isPlaying = soundPlayer?.isPlaying
+        {
+            if (isPlaying)
+            {
+                soundPlayer?.pause()
+            }
+            else
+            {
+                soundPlayer?.play()
+            }
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+         private func loadAudioFile() -> Void
+         {
             
+            if let soundURL = NSDataAsset(name: "")
+            {
+                Do
+                    {
+                        try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayBack)
+                        try! AVAudioSession.sharedInstance().setAtive(true)
+                        
+                        try soundPlayer = AVAudioPlayer(data: soundURL.data, fileTypeHint:AVFileType.mp3.rawValue)
+                        soundSlider.maximumVAlue = Float ((soundPlayer?.duration)!)
+                        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector:(#selector(self.updatesSlider)), userInfo: nill, repeats: true)
+                    }
+                catch
+                {
+                    print("Audio File Load Error")
+                }
+            }
 }
 
